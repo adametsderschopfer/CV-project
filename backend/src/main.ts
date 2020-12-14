29fc92329f0b34ec907c7da3528974f928503358
Adametsofficial <av.styledev@gmail.com>
2020-12-14 22:32:28 +0200

    feet(CORS): added supporting cors and write a doc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,29 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /* 
+    Supporting (CORS)
+      Cross-Origin Resource Sharing  
+  */
+
+  app.enableCors({
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'X-Access-Token',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+
+  /* 
+    App Listening
+  */
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
