338e78e3ea269b95d616dcd1c9da03e65bcf9f44
Adametsofficial <av.styledev@gmail.com>
2020-12-16 22:46:22 +0200

    feet: coonect to db and added modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminModule } from './components/admin/admin.module';
+import { PortfolioModule } from './components/portfolio/portfolio.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), AdminModule, PortfolioModule],
   controllers: [],
   providers: [],
 })
