507ef916d3e40ccc93751255b63c1dc708eb9e38
Adametsofficial <av.styledev@gmail.com>
2020-12-16 22:51:59 +0200

    feet: added signin and main(default page) route

---
+++ backend/src/components/admin/admin.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Render,
+  Res,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { AuthGuard } from './guards/Auth.guard';
+import { LoginDto } from './dto/login.dto';
+import { AuthException } from './exceptions/Auth.exception';
+import { Response } from 'express';
+
+@Controller('admin')
+export class AdminController {
+  constructor(private readonly _adminService: AdminService) {}
+
+  @Post('/signin')
+  SignIn(@Body() body: LoginDto, @Res() response: Response): Promise<never> {
+    return this._adminService.SignIn(body, response);
+  }
+
+  @Get('/')
+  @Render('Main')
+  @UseGuards(AuthGuard)
+  @UseFilters(AuthException)
+  Main() {
+    return;
+  }
+}
