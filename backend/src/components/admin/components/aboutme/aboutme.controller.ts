6971b49b0cc25e219d0a088fae0f42797c94b6c1
Adametsofficial <av.styledev@gmail.com>
2020-12-17 01:01:52 +0200

    feet(AboutMe): added about me page

--- backend/src/components/admin/components/aboutme/aboutme.controller.ts
+++ backend/src/components/admin/components/aboutme/aboutme.controller.ts
@@ -1,10 +1,30 @@
-import { Controller, Get, Patch, Render } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Redirect,
+  Render,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
+import { AboutMeService } from './aboutme.service';
+import { UpdateAboutmeDto } from './dto/UpdateContent.dto';
+import { TitlePage } from './../../dto/TitlePage.dto';
+import { AuthException } from './../../exceptions/Auth.exception';
+import { AuthGuard } from './../../guards/Auth.guard';
 
 @Controller('admin/aboutme')
+@UseGuards(AuthGuard)
+@UseFilters(AuthException)
 export class AboutMeController {
+  constructor(private readonly _aboutmeService: AboutMeService) {}
+
   @Get('/')
   @Render('Aboutme')
-  Page() {}
+  Page(): Promise<UpdateAboutmeDto & TitlePage> {
+    return this._aboutmeService.page();
+  }
 
   @Patch('/edit')
   Edit() {}
--- backend/src/components/admin/components/aboutme/aboutme.service.ts
+++ backend/src/components/admin/components/aboutme/aboutme.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Aboutme } from './models/Aboutme.entity';
+import { UpdateAboutmeDto } from './dto/UpdateContent.dto';
+import { TitlePage } from './../../dto/TitlePage.dto';
 
 @Injectable()
-export class AboutMeService {}
+export class AboutMeService {
+  constructor(
+    @InjectRepository(Aboutme)
+    readonly _aboutme: Repository<Aboutme>,
+  ) {}
+
+  async page(): Promise<UpdateAboutmeDto & TitlePage> {
+    const [{ content, projects_count, work_exp }] = await this._aboutme.find();
+
+    return {
+      title: 'About Me',
+      content,
+      projects_count,
+      work_exp,
+    };
+  }
+
+  async edit(data: UpdateAboutmeDto): Promise<any> {
+
+    return;
+  }
+}
