3631f6781edbd949b3a2705723b23634896ea627
Adametsofficial <av.styledev@gmail.com>
2020-12-17 22:25:01 +0200

    feet: project data transfer object

---
+++ backend/src/Dto/Project/Project.dto.ts
@@ -0,0 +1,7 @@
+export class ProjectDto {
+  readonly id?: number;
+  readonly title?: string;
+  readonly description?: string;
+  readonly repo_link?: string;
+  readonly website_link?: string;
+}
