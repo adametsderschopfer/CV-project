ab8a4e0a1e45e4bffbd0fe2359e72938becf7c69
Adametsofficial <av.styledev@gmail.com>
2020-12-22 02:21:06 +0200

    refactor: add interface for From component

--- backend/src/components/admin/views/interfaces/index.ts
+++ backend/src/components/admin/views/interfaces/index.ts
@@ -2,10 +2,18 @@ export interface Props {
   children: JSX.Element[] | JSX.Element | React.ReactNode;
   title?: string;
   err?: string;
+  views?: number;
 }
 
 export interface HeadTitleProps {
   title?: string;
   margin?: 1 | 2 | 3;
   el: 'h1' | 'h2' | 'h3';
+}
+
+export interface IForm {
+  children: JSX.Element[] | JSX.Element | React.ReactNode;
+  title: string;
+  to: string;
+  formMore?: any | any[];
 }
