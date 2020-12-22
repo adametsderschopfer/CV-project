7ff540990ae881365dfc1cf1b2dd9672714af4d7
Adametsofficial <av.styledev@gmail.com>
2020-12-22 02:23:05 +0200

    feet(view): add view service

---
+++ backend/src/services/views/views.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ViewsEntity } from 'src/Models/Views.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ViewsService {
+  constructor(
+    @InjectRepository(ViewsEntity)
+    private readonly _view: Repository<ViewsEntity>,
+  ) {}
+
+  async get(): Promise<number> {
+    const [{ views }] = await this._view.find();
+
+    return views;
+  }
+
+  async view(): Promise<void> {
+    await this._view.update('1', { views: (await this.get()) + 1 });
+  }
+}
