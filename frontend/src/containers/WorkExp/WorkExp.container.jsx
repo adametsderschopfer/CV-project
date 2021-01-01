14249fa35a4a96dd625c17443b30a7e3d003f889
Adametsofficial <av.styledev@gmail.com>
2021-01-01 04:05:39 +0200

    feet(WORKEXP): added WorkExp component

---
+++ frontend/src/containers/WorkExp/WorkExp.container.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getDataAction,
+  GET_REFERENCES,
+} from "../../StateManager/ducks/portfolio";
+
+import { Loader } from "../../components/Loader/Loader.components";
+
+import "./WorkExp.scss"
+
+export function WorkExp() {
+  return (
+    <main className="workexp">
+
+    </main>
+  )
+}
