d528e63e956b2fb4aeb2a49b9bac5551f2bfd44b
Adametsofficial <av.styledev@gmail.com>
2021-01-04 15:13:15 +0200

    build(Certs): finished create container

--- frontend/src/containers/Certs/Certs.container.jsx
+++ frontend/src/containers/Certs/Certs.container.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getDataAction, GET_CERTS } from "../../StateManager/ducks/portfolio";
 
 import { Loader } from "../../components/Loader/Loader.components";
+import zoomInImg from "../../assets/images/zoom-in.svg";
 
 import "./Certs.scss";
 
@@ -22,5 +23,27 @@ export function Certs() {
     }
   }, [certs, nothing, dispatch]);
 
-  return <main className="certs"></main>;
+  return (
+    <main className="certs">
+      <div className="certs--title title--page">Сертификаты</div>
+
+      <div className="certs--content" uk-grid uk-lightbox="animation: slide">
+        {(loading && <Loader />) ||
+          (!nothing &&
+            certs.length &&
+            certs.map((i, idx) => (
+              <a className="uk-inline certs--content__element" href={i.imglink}>
+                <div className="certs--content__element-hovered">
+                  <img src={zoomInImg} alt="" />
+                </div>
+                <img
+                  src={i.imglink}
+                  alt=""
+                  className="certs--content__element-img"
+                />
+              </a>
+            ))) || <div>Сертификатов пока нет {":("}</div>}
+      </div>
+    </main>
+  );
 }
