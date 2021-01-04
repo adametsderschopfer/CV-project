e2d604967c66e40d88d860196fa629a0304d50de
Adametsofficial <av.styledev@gmail.com>
2021-01-04 15:22:41 +0200

    build(Books): finished create component

--- frontend/src/containers/Books/Books.container.jsx
+++ frontend/src/containers/Books/Books.container.jsx
@@ -1,12 +1,10 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  getDataAction, GET_BOOKS, 
-} from "../../StateManager/ducks/portfolio";
+import { getDataAction, GET_BOOKS } from "../../StateManager/ducks/portfolio";
 
 import { Loader } from "../../components/Loader/Loader.components";
 
-import "./Books.scss"
+import "./Books.scss";
 
 export function Books() {
   const {
@@ -24,10 +22,63 @@ export function Books() {
     }
   }, [books, nothing, dispatch]);
 
+  function renderBooks() {
+    const readebleBooks = books.filter((i) => i.isReaded === true);
+    const notReadebleBooks = books.filter((i) => i.isReaded !== true);
+
+    const _renderBooks = (arr) =>
+      arr.map((i, idx) => (
+        <div
+          className="books--content-readeble_container--els__element"
+          key={idx}
+        >
+          <img
+            src={i.img}
+            alt=""
+            className="books--content-readeble_container--els__element-img"
+          />
+        </div>
+      ));
+
+    return (
+      <>
+        {(readebleBooks.length && (
+          <div className="books--content-readeble_container">
+            <div className="books--content-readeble_container-title">
+              Прочитанные
+            </div>
+            <div className="books--content-readeble_container--els">
+              {_renderBooks(readebleBooks)}
+            </div>
+          </div>
+        )) ||
+          ""}
+        {(notReadebleBooks.length && (
+          <div className="books--content-readeble_container">
+            <div className="books--content-readeble_container-title">
+              В очереди на чтение
+            </div>
+
+            <div className="books--content-readeble_container--els">
+              {_renderBooks(notReadebleBooks)}
+            </div>
+          </div>
+        )) ||
+          ""}
+      </>
+    );
+  }
 
   return (
     <main className="books">
+      <div className="books--title title--page">Книги</div>
 
+      <div className="books--content">
+        {(loading && <Loader />) ||
+          (!nothing && books.length && renderBooks()) || (
+            <div>Книг пока нет</div>
+          )}
+      </div>
     </main>
-  )
+  );
 }
