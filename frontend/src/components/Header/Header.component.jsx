2d13b6cac99897bf30242fa4b05870875d452d82
Adametsofficial <av.styledev@gmail.com>
2021-01-04 15:14:44 +0200

    refactor: remove animation

--- frontend/src/components/Header/Header.component.jsx
+++ frontend/src/components/Header/Header.component.jsx
@@ -1,7 +1,3 @@
-import { useEffect, useState } from "react";
-
-import anime from "animejs";
-
 import { HeaderMobile } from "./Header-mobile.component";
 import { HeaderNavLinks } from "./HeaderNavLinks.component";
 import { HeaderSocial } from "./Header-social.component";
@@ -10,28 +6,7 @@ import useWindowDimensions from "../../hooks/useWindowDimensions";
 import "./Header.scss";
 
 export function Header() {
-  const [countOne, setCountOne] = useState(0);
   const { width } = useWindowDimensions();
-
-  useEffect(() => {
-    if (countOne === 0) {
-      if (width >= 700) {
-        anime({
-          targets: ".animateSvg",
-          strokeDashoffset: [anime.setDashoffset, 1],
-          easing: "linear",
-          duration: 1000,
-          delay: function (el, i) {
-            return i * 350;
-          },
-        });
-
-        setCountOne(1);
-      }
-    } else if (width < 700) {
-      setCountOne(0);
-    }
-  }, [width, countOne]);
 
   if (width <= 700) {
     return (
