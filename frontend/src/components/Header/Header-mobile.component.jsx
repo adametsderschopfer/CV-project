80a9abc3edd203b0cd463001d4f97096b3efceb0
Adametsofficial <av.styledev@gmail.com>
2021-01-10 19:33:58 +0200

    fix: dropdown menu not hideble when cursor not over it

--- frontend/src/components/Header/Header-mobile.component.jsx
+++ frontend/src/components/Header/Header-mobile.component.jsx
@@ -4,15 +4,24 @@ import { HeaderSocial } from "./Header-social.component";
 import "./Header.scss";
 
 export function HeaderMobile() {
+  function openMore() {
+    return window.UIkit.dropdown(
+      document.getElementById("dropdown-menu")
+    ).show();
+  }
+
   return (
     <div className="header--mobile">
       <HeaderSocial classes="header--mobile__menu-svg" />
-      <button className="header--mobile__button">
+      <button className="header--mobile__button" onClick={openMore}>
         <span uk-icon="icon: grid; ratio: 1.5"></span>
       </button>
 
       <div uk-drop="pos: bottom-justify; animation: uk-animation-slide-top-small; duration: 1000; mode: click">
-        <div className="uk-card uk-card-body uk-card-default">
+        <div
+          id="dropdown-menu"
+          className="uk-card uk-card-body uk-card-default"
+        >
           <HeaderNavLinks classes="header--mobile__menu" />
         </div>
       </div>
--- frontend/src/components/Header/HeaderNavLinks.component.jsx
+++ frontend/src/components/Header/HeaderNavLinks.component.jsx
@@ -1,6 +1,28 @@
 import { NavLink } from "react-router-dom";
 
 export function HeaderNavLinks({ classes = "" }) {
+  function openMore() {
+    return window.UIkit.dropdown(
+      document.getElementById("dropdown-more")
+    ).show();
+  }
+
+  function closeMore(isdesk) {
+    return () => {
+      let elem;
+
+      if (isdesk) {
+        elem = document.getElementById("dropdown-more");
+      } else {
+        elem = document.getElementById("dropdown-menu");
+      }
+
+      window.UIkit.dropdown(document.getElementById("dropdown-more")).hide();
+
+      window.UIkit.dropdown(elem).hide();
+    };
+  }
+
   return (
     <nav className={`header--menu--nav ${classes}`}>
       <NavLink
@@ -8,6 +30,7 @@ export function HeaderNavLinks({ classes = "" }) {
         to="/"
         exact={true}
         className="header--menu__item"
+        onClick={closeMore()}
       >
         Главная
       </NavLink>
@@ -16,6 +39,7 @@ export function HeaderNavLinks({ classes = "" }) {
         activeClassName="header--menu__item-active"
         to="/about"
         className="header--menu__item"
+        onClick={closeMore()}
       >
         Обо мне
       </NavLink>
@@ -24,22 +48,25 @@ export function HeaderNavLinks({ classes = "" }) {
         activeClassName="header--menu__item-active"
         to="/contacts"
         className="header--menu__item"
+        onClick={closeMore()}
       >
         Контакты
       </NavLink>
       <button
         className="header--menu__item header--menu__item--more__button"
         type="button"
+        onClick={openMore}
       >
         Более
       </button>
-      <div uk-dropdown="pos: bottom">
+      <div uk-dropdown="pos: bottom" id="dropdown-more">
         <ul className="uk-nav uk-dropdown-nav">
           <li>
             <NavLink
               activeClassName="header--menu__item-active"
               to="/workexp"
               className="header--menu__item header--menu__item--more__item"
+              onClick={closeMore(true)}
             >
               Опыт работы
             </NavLink>
@@ -49,6 +76,7 @@ export function HeaderNavLinks({ classes = "" }) {
               activeClassName="header--menu__item-active"
               to="/projects"
               className="header--menu__item header--menu__item--more__item"
+              onClick={closeMore(true)}
             >
               Проекты
             </NavLink>
@@ -58,6 +86,7 @@ export function HeaderNavLinks({ classes = "" }) {
               activeClassName="header--menu__item-active"
               to="/technologies"
               className="header--menu__item header--menu__item--more__item"
+              onClick={closeMore(true)}
             >
               Умения и Технологии
             </NavLink>
@@ -67,6 +96,7 @@ export function HeaderNavLinks({ classes = "" }) {
               activeClassName="header--menu__item-active"
               to="/certs"
               className="header--menu__item header--menu__item--more__item"
+              onClick={closeMore(true)}
             >
               Сертификаты
             </NavLink>
@@ -76,6 +106,7 @@ export function HeaderNavLinks({ classes = "" }) {
               activeClassName="header--menu__item-active"
               to="/books"
               className="header--menu__item header--menu__item--more__item"
+              onClick={closeMore(true)}
             >
               Книги
             </NavLink>
@@ -85,6 +116,7 @@ export function HeaderNavLinks({ classes = "" }) {
               activeClassName="header--menu__item-active"
               to="/references"
               className="header--menu__item header--menu__item--more__item"
+              onClick={closeMore(true)}
             >
               References
             </NavLink>
